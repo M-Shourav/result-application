eeae5ccefb1c66b0178e5e3e65eed2642fad1ea2
M-Shourav <muhammadshourav625@gmail.com>
2025-10-24 20:59:38 +0600

    user structure done

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -1,5 +1,9 @@
 import asyncHandler from "express-async-handler";
 import userModel from "../models/userModel.js";
+import cloudinary from "../utils/Cloudinary.js";
+import generateToken from "../utils/generateToken.js";
+import validator from "validator";
+import jwt from "jsonwebtoken";
 
 const registerUser = asyncHandler(async (req, res) => {
   try {
@@ -21,11 +25,30 @@ const registerUser = asyncHandler(async (req, res) => {
       });
     }
 
+    // Upload avatar if provided
+    let avatar = {
+      url: "https://res.cloudinary.com/drswdtncv/image/upload/v1753807270/images_wuqnyi.jpg", // default
+      public_id: "",
+    };
+
+    if (req.file) {
+      const file = cloudinary.uploader.upload(req.file.path, {
+        resource_type: "auto",
+        folder: "avatars",
+      });
+
+      avatar = {
+        url: (await file).secure_url,
+        public_id: (await file).public_id,
+      };
+    }
+
     const user = await userModel.create({
       name,
       email,
       password,
       role,
+      avatar: avatar,
     });
 
     return res.json({
@@ -36,6 +59,7 @@ const registerUser = asyncHandler(async (req, res) => {
         name: user.name,
         email: user.email,
         role: user.role,
+        avatar: user.avatar,
       },
     });
   } catch (error) {
@@ -48,6 +72,39 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 const loginUser = asyncHandler(async (req, res) => {
   try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "user Doesn't exits",
+      });
+    }
+
+    const isMatch = await user.matchPassword(password);
+
+    if (isMatch) {
+      const token = generateToken(user._id);
+
+      res.cookie("token", token, {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: false,
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+      });
+
+      res.json({
+        success: true,
+        message: "User logged in successfully",
+      });
+    } else {
+      return res.json({
+        success: false,
+        message: "Invalid credential try again!",
+      });
+    }
   } catch (error) {
     console.log("login error:", error);
     return res.json({
@@ -58,6 +115,15 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 const logoutUser = asyncHandler(async (req, res) => {
   try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      sameSite: "lax",
+      secure: false,
+    });
+    res.json({
+      success: true,
+      message: "user logout successfully",
+    });
   } catch (error) {
     console.log("logout error:", error);
     return res.json({
@@ -68,6 +134,24 @@ const logoutUser = asyncHandler(async (req, res) => {
 });
 const deleteUser = asyncHandler(async (req, res) => {
   try {
+    const user = await userModel.findById(req.params.id);
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "user not found try again",
+      });
+    }
+
+    // delete image form cloudinary
+    if (user.avatar.public_id) {
+      await cloudinary.uploader.destroy(user?.avatar.public_id);
+    }
+
+    await userModel.findByIdAndDelete(user);
+    return res.json({
+      success: true,
+      message: "user remove successfully",
+    });
   } catch (error) {
     console.log("delete user error:", error);
     return res.json({
@@ -78,6 +162,53 @@ const deleteUser = asyncHandler(async (req, res) => {
 });
 const updateUser = asyncHandler(async (req, res) => {
   try {
+    const { name, email } = req.body;
+    const user = await userModel.findById(req.params.id);
+
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "user not found.",
+      });
+    }
+
+    if (req.file) {
+      if (user?.avatar && user?.avatar.public_id) {
+        await cloudinary.uploader.destroy(user?.avatar.public_id);
+      }
+
+      const uploadFile = await cloudinary.uploader.upload(req.file?.path, {
+        folder: "avatars",
+      });
+
+      user.avatar = {
+        url: uploadFile?.secure_url,
+        public_id: uploadFile.public_id,
+      };
+    }
+    if (name) user.name = name;
+    if (email) {
+      if (!validator.isEmail(email)) {
+        return res.json({
+          success: false,
+          message: "invalid email address, try valid email address",
+        });
+      }
+      user.email = email;
+    }
+
+    // update user
+    const update = await user.save();
+    return res.json({
+      success: true,
+      message: "user data update successfully",
+      user: {
+        _id: update._id,
+        name: update.name,
+        email: update.email,
+        avatar: update.avatar,
+      },
+    });
   } catch (error) {
     console.log("User update error:", error);
     return res.json({
@@ -88,6 +219,14 @@ const updateUser = asyncHandler(async (req, res) => {
 });
 const GetUserList = asyncHandler(async (req, res) => {
   try {
+    const total = await userModel.countDocuments({});
+    const user = await userModel.find().select("-password");
+
+    return res.json({
+      success: true,
+      total,
+      user,
+    });
   } catch (error) {
     console.log("Get user list error:", error);
     return res.json({
@@ -98,6 +237,13 @@ const GetUserList = asyncHandler(async (req, res) => {
 });
 const SingleUser = asyncHandler(async (req, res) => {
   try {
+    const token = req.cookies?.token;
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await userModel.findOne(decode._id).select("-password");
+    return res.json({
+      success: true,
+      user,
+    });
   } catch (error) {
     console.log("User error:", error);
     return res.json({
--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -8,14 +8,15 @@ import {
   SingleUser,
   updateUser,
 } from "../controller/UserController.js";
+import upload from "../middlewares/multer.js";
 
 const userRouter = Router();
 
-userRouter.post("/register", registerUser);
+userRouter.post("/register", upload.single("avatar"), registerUser);
 userRouter.post("/login", loginUser);
 userRouter.post("/logout", logoutUser);
 userRouter.post("/remove-user/:id", deleteUser);
-userRouter.put("/user-update/:id", updateUser);
+userRouter.put("/user-update/:id", upload.single("avatar"), updateUser);
 userRouter.get("/user-list", GetUserList);
 userRouter.get("/single-user", SingleUser);
 
