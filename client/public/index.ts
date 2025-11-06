cf11b047df3c39f7449a23e6311a0a154568d6b5
M-Shourav <muhammadshourav625@gmail.com>
2025-11-06 23:48:28 +0600

    academics

--- client/public/index.ts
+++ client/public/index.ts
@@ -1,6 +1,24 @@
 import Logo from "./images/logo.png";
 import BannerImage_0 from "./images/Banner-image.png";
-import BannerImage_1 from "./images/Banner-image2.png";
 import EduBanner from "./images/Edu-banner.jpg";
-
-export { Logo, BannerImage_0, BannerImage_1, EduBanner };
+import Class_Room from "./images/class.jpg";
+import Departments_image from "./images/premium_photo-1663089905440-78880eab98c1.jpeg";
+import Subject_overview from "./images/Subject_overview.jpg";
+import Examination_image from "./images/premium_photo-1664299728921-02e66e6146b4.jpeg";
+import Academic_img from "./images/Cariculam.jpeg";
+import Curricular_img from "./images/premium_photo-1708696237438-275bdbf74470.jpeg";
+import Online_class from "./images/premium_photo-1683124226167-6d79bd64d725.jpeg";
+import Awards_img from "./images/photo-1594122230689-45899d9e6f69.jpeg";
+export {
+  Logo,
+  BannerImage_0,
+  EduBanner,
+  Class_Room,
+  Departments_image,
+  Subject_overview,
+  Examination_image,
+  Academic_img,
+  Curricular_img,
+  Online_class,
+  Awards_img,
+};
