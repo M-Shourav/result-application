3dcb86f8cf6e853466759e1d65df016296588474
M-Shourav <muhammadshourav625@gmail.com>
2025-11-21 23:51:24 +0600

    check

--- client/components/NavbarWrapper.tsx
+++ client/components/NavbarWrapper.tsx
@@ -29,7 +29,7 @@ const Navbar: React.FC = () => {
   useEffect(() => {
     const handleScroll = () => {
       const currentScroll = window.scrollY;
-      if (currentScroll > 100) {
+      if (currentScroll > 50) {
         setShow(true); // scroll 50px por navbar show
       } else {
         setShow(false); // top e scroll korle hide
@@ -41,13 +41,171 @@ const Navbar: React.FC = () => {
   }, []);
 
   return (
-    <div
-      className={`py-5 fixed top-0 left-0 w-full z-50 bg-white transition-transform duration-500 shadow-md overflow-hidden ${
-        show ? "translate-y-0" : "-translate-y-full"
-      }`}
-    >
-      <Container className={"py-10"}>Navbar</Container>
-    </div>
+    <Container>
+      <div
+        className={`py-5 max-w-screen-[1280px] mx-auto px-4 md:px-8 fixed top-0 left-0 w-full  z-50 bg-white/95 backdrop-blur transition-all duration-500 shadow-md ${
+          show ? "translate-y-0" : "-translate-y-full"
+        }`}
+      >
+        <div className="flex items-center justify-between">
+          <Link href={"/"}>
+            <Image
+              src={Logo}
+              alt="logo"
+              className="w-[120px] md:w-[200px] h-auto"
+            />
+          </Link>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Menu className="text-green-600 cursor-pointer -mt-2" size={30} />
+            </SheetTrigger>
+            <SheetContent className="overflow-y-auto max-h-screen">
+              <SheetHeader className="bg-green-50">
+                <SheetTitle></SheetTitle>
+              </SheetHeader>
+              <div className="flex flex-col gap-4 px-4">
+                <div className="p-2 pb-3 border-b border-b-gray-200 -mt-3">
+                  <Link
+                    href={"/"}
+                    className="text-sm font-semibold hover:text-green-600 duration-300"
+                  >
+                    মূলপাতা
+                  </Link>
+                </div>
+                <div className="px-2 border-b border-b-gray-200">
+                  <Accordion type="single" collapsible className="w-full">
+                    <AccordionItem value="item-1">
+                      <AccordionTrigger className="text-sm font-semibold py-0 pb-3  ">
+                        পরিচিতি
+                      </AccordionTrigger>
+                      <AccordionContent className=" flex flex-col gap-4">
+                        <Link
+                          href={"/"}
+                          className="text-sm font-semibold text-gray-600 p-2 pb-4 border-b border-b-gray-200 hover:text-green-500 duration-500"
+                        >
+                          প্রতিষ্ঠানের পরিচিতি
+                        </Link>
+                        <Link
+                          href={"/"}
+                          className="text-sm font-semibold text-gray-600 p-2 -mt-3 pb-3 border-b border-b-gray-200 hover:text-green-500 duration-500"
+                        >
+                          বিদ্যালয়ের তথ্যাদি
+                        </Link>
+                        <Link
+                          href={"/"}
+                          className="text-sm font-semibold text-gray-600  px-2  hover:text-green-500 duration-500 "
+                        >
+                          স্কুলের পরিচালকের বাণী
+                        </Link>
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
+                </div>
+                <div className="px-2 border-b border-b-gray-200">
+                  <Accordion type="single" collapsible className="w-full">
+                    <AccordionItem value="item-1">
+                      <AccordionTrigger className="text-sm font-semibold py-0 pb-4">
+                        জনবল
+                      </AccordionTrigger>
+                      <AccordionContent className=" flex flex-col gap-4">
+                        <Link
+                          href={"/"}
+                          className="text-sm font-semibold text-gray-600 p-2 pb-3 border-b border-b-gray-200 hover:text-green-500 duration-500"
+                        >
+                          শিক্ষক/শিক্ষিকা
+                        </Link>
+                        <Link
+                          href={"/"}
+                          className="text-sm font-semibold text-gray-600 px-2  hover:text-green-500 duration-500"
+                        >
+                          স্কুল কমিটি সদস্যবৃন্দ
+                        </Link>
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
+                </div>
+                <div className="px-2 border-b border-b-gray-200">
+                  <Accordion type="single" collapsible className="w-full">
+                    <AccordionItem value="item-1">
+                      <AccordionTrigger className="text-sm font-semibold py-0 pb-3">
+                        শিক্ষার্থী
+                      </AccordionTrigger>
+                      <AccordionContent className=" flex flex-col gap-4">
+                        <Link
+                          href={"/"}
+                          className="text-sm font-semibold text-gray-600 p-2 pb-3 border-b border-b-gray-200 hover:text-green-500 duration-500"
+                        >
+                          অধ্যয়নর‍ত শিক্ষার্থীর সংখ্যা
+                        </Link>
+                        <Link
+                          href={"/"}
+                          className="text-sm font-semibold text-gray-600 px-2 hover:text-green-500 duration-500"
+                        >
+                          অধ্যয়নর‍ত শিক্ষার্থীর তালিকা
+                        </Link>
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
+                </div>
+                <div className="px-2 border-b border-b-gray-200">
+                  <Accordion type="single" collapsible className="w-full">
+                    <AccordionItem value="item-1">
+                      <AccordionTrigger className="text-sm font-semibold py-0 pb-4">
+                        পরিক্ষার ফলাফল
+                      </AccordionTrigger>
+                      <AccordionContent className=" flex flex-col gap-4">
+                        <Link
+                          href={"/"}
+                          className="text-sm font-semibold text-gray-600 p-2 pb-3 border-b border-b-gray-200 hover:text-green-500 duration-500"
+                        >
+                          পাবলিক পরীক্ষা
+                        </Link>
+                        <Link
+                          href={"/result"}
+                          className="text-sm font-semibold text-gray-600 px-2  hover:text-green-500 duration-500"
+                        >
+                          বিদ্যালয়ের অভ্যন্তরীণ পরীক্ষা
+                        </Link>
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
+                </div>
+                <div className="p-2 border-b -mt-3 border-b-gray-200">
+                  <Link href={"/"} className="text-sm font-semibold">
+                    ছবির গ্যালারী
+                  </Link>
+                </div>
+                <div className="p-2 -mt-3 border-b border-b-gray-200">
+                  <Link href={"/"} className="text-sm font-semibold">
+                    যোগাযোগ
+                  </Link>
+                </div>
+              </div>
+
+              <SheetFooter>
+                <div className="flex flex-wrap items-center justify-center gap-x-4 gap-y-2 mb-10">
+                  <div className="flex items-center gap-1">
+                    <p className="text-muted-foreground">EIIN No:</p>
+                    <p className="text-black">121314</p>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <p className="text-muted-foreground"> School code:</p>
+                    <p className="text-black">123456</p>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <p className="text-muted-foreground">Reg No:</p>
+                    <p className="text-black">121314151617</p>
+                  </div>
+                </div>
+                <Button className="bg-green-500 py-6 text-xl hover:bg-green-600 duration-500">
+                  Login
+                </Button>
+              </SheetFooter>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+    </Container>
   );
 };
 
